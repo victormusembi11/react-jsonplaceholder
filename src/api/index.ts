01df4a4bfe3b2721b14f7f6974973a5650483c4a
victormusembi11 <victor.musembi11@gmail.com>
2024-05-14 17:23:04 +0300

    feat: Add fetchPost and fetchComments API functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { Post, User } from "@/types";
+import { Post, User, Comment } from "@/types";
 
 export const fetchPosts = async () => {
   const { data } = await axios.get<Post[]>(
@@ -13,6 +13,22 @@ export const fetchPosts = async () => {
 export const fetchUsers = async () => {
   const { data } = await axios.get<User[]>(
     "https://jsonplaceholder.typicode.com/users"
+  );
+
+  return data;
+};
+
+export const fetchPost = async (id: number) => {
+  const { data } = await axios.get<Post>(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+
+  return data;
+};
+
+export const fetchComments = async (id: number) => {
+  const { data } = await axios.get<Comment[]>(
+    `https://jsonplaceholder.typicode.com/posts/${id}/comments`
   );
 
   return data;
