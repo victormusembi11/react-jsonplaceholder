704de7641322cecf5c48fa0795fbd1596817aad2
victormusembi11 <victor.musembi11@gmail.com>
2024-05-14 12:22:31 +0300

    feat: Integrate react-router-dom for routing and add RouterProvider component

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Layout } from "./layout";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
