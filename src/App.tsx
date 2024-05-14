42ec94a1a5fe05e834281775f44966a0a3ef463d
victormusembi11 <victor.musembi11@gmail.com>
2024-05-14 10:16:54 +0300

    refactor: Add Button component and update App to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
-    <>
+    <div>
       <h1 className="underline">Hello world!</h1>
-    </>
+      <Button>Click me</Button>
+    </div>
   );
 }
 
